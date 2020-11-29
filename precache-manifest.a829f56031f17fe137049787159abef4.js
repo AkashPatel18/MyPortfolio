79ff580dc9cc1263a20fd40fe71eb478e0701a01
Akash <akashpatel18041999@gmail.com>
2020-11-29 22:25:03 +0530

    Updates

---
+++ precache-manifest.a829f56031f17fe137049787159abef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcbf833ff407095fd9ed6b3f68ce182",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "374f0645e6d5cf410f64",
+    "url": "/MyPortfolio/static/css/main.2204671f.chunk.css"
+  },
+  {
+    "revision": "9de5bb4c06357c519418",
+    "url": "/MyPortfolio/static/js/2.177afc56.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MyPortfolio/static/js/2.177afc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374f0645e6d5cf410f64",
+    "url": "/MyPortfolio/static/js/main.a56b3769.chunk.js"
+  },
+  {
+    "revision": "0104f0378fe4c4538e68",
+    "url": "/MyPortfolio/static/js/runtime-main.b029b7a3.js"
+  },
+  {
+    "revision": "33d6bdb0d53a4ecb04672b6cbba7836f",
+    "url": "/MyPortfolio/static/media/tp.33d6bdb0.jpg"
+  }
+]);
